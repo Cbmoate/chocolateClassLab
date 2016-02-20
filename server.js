66ee7af29f9257384b6d3a9dc8d963f88cd5f876
CB <cbmoate@gmail.com>
2016-02-20 10:23:05 -0500

    setup serving vars/connections

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require('express');
-var expressHandlebars = require('express-handlebars');
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('user_info_db', 'root', "");
-var mysql = require('mysql');
-var PORT = process.env.NODE_ENV || 3000;
 var app = express();
+var PORT = process.env.NODE_ENV || 3000;
+
+var mysql = require('mysql');
+
+var expressHandlebars = require('express-handlebars');
+app.enging('handlebars', expressHandlebars({
+  defaultLayout: 'main'
+}));
+
+var Sequelize = require('sequelize');
+var connection = new Sequelize('chocolate_db', 'root');
+
+app.get('/', function(req, res){
+  res.render('chocolife');
+});
+
+connection.sync().then(function(){
+  app.listen(PORT, function(){
+    console.log('Online on:' +PORT)
+  });
+});
